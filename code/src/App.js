900ba023dbf9ee298af0acdf6ac331f3e5d1946f
Mamite Andersson <71432371+mamite100@users.noreply.github.com>
2022-02-22 23:42:53 +0100

    Update App.js

--- code/src/App.js
+++ code/src/App.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-=======
-import React,from 'react';
->>>>>>> 0ee640bc425bd1a7e995a977c115209fcb319f7f
 import Form from 'components/Form';
 export const App = () => {
     return (
